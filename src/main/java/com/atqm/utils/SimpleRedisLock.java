53cdad8de8b184d2da315def2225d06e683842b8
Qimin <Qimin@atqm.com>
2022-03-20 10:37:43 +0800

    优惠券秒杀功能（redis分布式锁）优化锁，解决当某一业务被阻塞，锁到期释放后其他线程获得锁，被阻塞业务完成，将其他线程的锁进行了释放，

--- src/main/java/com/atqm/utils/SimpleRedisLock.java
+++ src/main/java/com/atqm/utils/SimpleRedisLock.java
@@ -1,5 +1,6 @@
 package com.atqm.utils;
 
+import cn.hutool.core.lang.UUID;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.util.concurrent.TimeUnit;
@@ -10,6 +11,7 @@ public class SimpleRedisLock implements ILock{
     private String name;
     private StringRedisTemplate stringRedisTemplate;
     private static final String KEY_PREFIX = "lock";
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true)+"-";
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
@@ -18,21 +20,29 @@ public class SimpleRedisLock implements ILock{
 
     @Override
     public boolean tryLock(Long timeoutSec) {
-            String key = KEY_PREFIX + name;
+        String key = KEY_PREFIX + name;
 
             // 获取线程表示id
-            long threadId = Thread.currentThread().getId();
+        String threadId = ID_PREFIX+Thread.currentThread().getId();
 
             // 获取锁
-            Boolean success = stringRedisTemplate.opsForValue().setIfAbsent(key, threadId + "", timeoutSec, TimeUnit.SECONDS);
+        Boolean success = stringRedisTemplate.opsForValue().setIfAbsent(key, threadId, timeoutSec, TimeUnit.SECONDS);
             // 返回获取锁的结果
-            return success;
+        return success;
     }
 
     @Override
     public void unlock() {
-            String key = KEY_PREFIX + name;
+        String key = KEY_PREFIX + name;
 
+        // 获取线程表示id
+        String threadId = ID_PREFIX+Thread.currentThread().getId();
+                // 判断是否是当前线程的锁
+        String id = stringRedisTemplate.opsForValue().get(key);
+
+        // 当前的锁是自己线程的才进行释放（删除）
+        if(threadId.equals(id)){
             stringRedisTemplate.delete(key);
+        }
     }
 }
